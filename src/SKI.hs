623a0d82683d4338805ef22fb9ba60b088900f3d
AnnikaRings <annika.rings@optellum.com>
2024-04-17 22:50:42 +0100

    added show instance

--- src/SKI.hs
+++ src/SKI.hs
@@ -1,3 +1,9 @@
 module SKI (SKI (..)) where
 
-data SKI = S | K | I | App [SKI]  deriving (Show, Eq)
+data SKI = S | K | I | App [SKI] deriving (Eq)
+
+instance Show SKI where
+  show S = "S"
+  show K = "K"
+  show I = "I"
+  show (App xs) = concatMap show xs
